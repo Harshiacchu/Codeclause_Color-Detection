4d953c1a0aa681dc807920e5378f0c0f226aa556
Harshiacchu <100066470+Harshiacchu@users.noreply.github.com>
2023-05-28 21:21:46 +0530

    Update color_detection.py

--- color_detection.py
+++ color_detection.py
@@ -2,34 +2,34 @@ import cv2
 import numpy as np
 import pandas as pd
 import argparse
-import sys
+# import sys
 
-sys.path.append('/usr/local/lib/python2.7/site-packages')
+# sys.path.append('/usr/local/lib/python2.7/site-packages')
 
 #Creating argument parser to take image path from command line
-ap = argparse.ArgumentParser()
-ap.add_argument('-i', '--image', required=True, help="Image Path")
-args = vars(ap.parse_args())
-img_path = args['image']
+arp = argparse.ArgumentParser()
+arp.add_argument('-i', '--image', required=True, help="Image Path")
+args = vars(arp.parse_args())
+imgpath = args['image']
 
 #Reading the image with opencv
-img = cv2.imread(img_path)
+img = cv2.imread(imgpath)
 
 #declaring global variables (are used later on)
 clicked = False
 r = g = b = xpos = ypos = 0
 
 #Reading csv file with pandas and giving names to each column
-index=["color","color_name","hex","R","G","B"]
-csv = pd.read_csv('colors.csv', names=index, header=None)
+ind=["color","color_name","hex","R","G","B"]
+csv = pd.read_csv('colors.csv', names=ind, header=None)
 
 #function to calculate minimum distance from all colors and get the most matching color
 def getColorName(R,G,B):
-    minimum = 10000
+    min = 10000
     for i in range(len(csv)):
         d = abs(R- int(csv.loc[i,"R"])) + abs(G- int(csv.loc[i,"G"]))+ abs(B- int(csv.loc[i,"B"]))
-        if(d<=minimum):
-            minimum = d
+        if(d<=min):
+            min = d
             cname = csv.loc[i,"color_name"]
     return cname
 
